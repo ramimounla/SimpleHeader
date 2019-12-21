0980dbcd0a4a1b999531cd58ddeb1e5e108d8e3f
Rami Mounla <Rami_Mounla@hotmail.com>
2019-12-21 23:44:14 +1300

    added subheader option

--- SimpleHeader/index.ts
+++ SimpleHeader/index.ts
@@ -1,13 +1,12 @@
-import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
 
 export class SimpleHeader implements ComponentFramework.StandardControl<IInputs, IOutputs> {
 
-	
+
 	/**
 	 * Empty constructor.
 	 */
-	constructor()
-	{
+	constructor() {
 
 	}
 
@@ -19,14 +18,26 @@ export class SimpleHeader implements ComponentFramework.StandardControl<IInputs,
 	 * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
 	 * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
 	 */
-	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement)
-	{
+	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement) {
 		// Add control initialization code
 		let divHeader: HTMLDivElement = document.createElement("div");
-		divHeader.innerText = context.parameters.headerText.raw || "";
+		let spanHeader: HTMLSpanElement = document.createElement("span");
 
-		if(((context.parameters.styleText.raw || "").trim().length > 0))
-			divHeader.style.cssText = context.parameters.styleText.raw || "";
+		spanHeader.innerText = context.parameters.headerText.raw || "";
+
+		if (((context.parameters.styleText.raw || "").trim().length > 0))
+			spanHeader.style.cssText = context.parameters.styleText.raw || "";
+
+		divHeader.appendChild(spanHeader);
+
+		if (((context.parameters.subHeaderText.raw || "").trim().length > 0)) {
+			let spanSubHeader: HTMLSpanElement = document.createElement("span");
+			spanSubHeader.innerText = context.parameters.subHeaderText.raw || "";
+
+			if (((context.parameters.subHeaderStyleText.raw || "").trim().length > 0))
+				spanSubHeader.style.cssText = context.parameters.subHeaderStyleText.raw || "";
+			divHeader.appendChild(spanSubHeader);
+		}
 
 		container.appendChild(divHeader);
 	}
@@ -36,8 +47,7 @@ export class SimpleHeader implements ComponentFramework.StandardControl<IInputs,
 	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
 	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
 	 */
-	public updateView(context: ComponentFramework.Context<IInputs>): void
-	{
+	public updateView(context: ComponentFramework.Context<IInputs>): void {
 		// Add code to update control view
 	}
 
@@ -45,8 +55,7 @@ export class SimpleHeader implements ComponentFramework.StandardControl<IInputs,
 	 * It is called by the framework prior to a control receiving new data. 
 	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
 	 */
-	public getOutputs(): IOutputs
-	{
+	public getOutputs(): IOutputs {
 		return {};
 	}
 
@@ -54,8 +63,7 @@ export class SimpleHeader implements ComponentFramework.StandardControl<IInputs,
 	 * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
 	 * i.e. cancelling any pending remote calls, removing listeners, etc.
 	 */
-	public destroy(): void
-	{
+	public destroy(): void {
 		// Add code to cleanup control if necessary
 	}
 }
